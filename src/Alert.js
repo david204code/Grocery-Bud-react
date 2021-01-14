896a37ea27e5abc3f651871f1f163d5118bb9e22
David Wu <david204code@gmail.com>
2021-01-14 21:04:57 +0000

    useEffect with clean up function for the alert on the Alert component

--- src/Alert.js
+++ src/Alert.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react'
 
 const Alert = ({ type, msg, removeAlert }) => {
+  
+  useEffect(() => {
+    // clean up function
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000)
+    return() => clearTimeout(timeout)
+  }, [])
   return (
     // template string in className
     <p className ={`alert alert-${type}`}>{msg}</p>
